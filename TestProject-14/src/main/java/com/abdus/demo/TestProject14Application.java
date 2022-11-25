f8371b776a688fee3d3296801696f580b01cfb64
Abdus <Abdus@DESKTOP-G5ROS0D>
2022-11-25 21:57:19 +0530

    GGGG

---
+++ TestProject-14/src/main/java/com/abdus/demo/TestProject14Application.java
@@ -0,0 +1,13 @@
+package com.abdus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProject14Application.class, args);
+	System.out.println();	}
+
+}
